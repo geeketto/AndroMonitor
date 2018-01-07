ddaa3fda8cd68539b991287bbd38b9a870c9d7fe
geeketto <h2o.al.97@gmail.com>
2018-01-07 21:10:49 +0100

    Add adb and host resolution code

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+### You can find the list of monitor with the command $ xrandr
 ### Uncomment what you are using
 ##Laptop
 #fisic="LVDS1"
@@ -11,8 +12,29 @@ fisic="eDP1"
 #fisic="HDM1"
 
 ## ADB bin
-adb_bin=/bin/adb_bin
+adb_bin=/bin/adb
 
 #echo $@
 
+## Find android resolution
+if [ -z "$device" ] ; then
+	device=$($adb_bin shell dumpsys window displays | grep init | cut -d'=' -f 2 | cut -d' ' -f 1)
+fi
+if [ -z "$device" ] ; then
+	echo "Can't read device resolution using adb"
+	exit 0
+else
+	## Device width and height
+	d_width=$(echo $device | cut -d'x' -f 1)
+	d_height=$(echo $device | cut -d'x' -f 2)
+	### echo $d_height
+	### echo $d_width
+fi
+
+## Find host resolution
+host=$(xdpyinfo  | grep 'dimensions:' | cut -d' ' -f 7)
+h_width=$(echo $host | cut -d'x' -f 1)
+h_height=$(echo $host | cut -d'x' -f 2)
+### echo $h_height
+### echo $h_width
 
