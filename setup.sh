ef360076fafb8353876053dce5b3556b67ad91c1
geeketto <h2o.al.97@gmail.com>
2018-01-07 20:18:30 +0100

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+### Uncomment what you are using
+##Laptop
+#fisic="LVDS1"
+##Modern Laptop
+fisic="eDP1"
+##VGA
+#fisic="VGA1"
+## HDMI
+#fisic="HDM1"
+
+## ADB bin
+adb_bin=/bin/adb_bin
+
+#echo $@
+
+
