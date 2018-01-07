59a1361ffe00296490e88e337e786a5a3cdbe36c
geeketto <h2o.al.97@gmail.com>
2018-01-07 22:38:12 +0100

    Full work

--- setup.sh
+++ setup.sh
@@ -15,6 +15,11 @@ fisic="eDP1"
 adb_bin=/bin/adb
 
 #echo $@
+## Parsen of manual resolution
+device=$(echo $@ | grep -Po '\d+x\d+')
+
+## Position of android divice left or right
+position=$(echo $@ | grep -Po '\--(left|right)' | grep -Po '\w+')
 
 ## Find android resolution
 if [ -z "$device" ] ; then
@@ -38,3 +43,39 @@ h_height=$(echo $host | cut -d'x' -f 2)
 ### echo $h_height
 ### echo $h_width
 
+## Build the modeline for the monitore config
+modeline=$(gtf $d_width $d_height 60 | grep "Modeline" | cut -d' ' -f 4-17)
+### echo $modeline
+mode=$(gtf $d_width $d_height 60 | grep "Modeline" | cut -d' ' -f 4-5)
+res=$(echo ${mode// \" })
+
+## Set the default position use for a new monitor
+if [ -z "$position" ] ; then
+	position="right"
+fi
+if [ "$position" = "left" ] ; then
+	xinerama="xinerama0"
+else
+	xinerama="xinerama1"
+fi
+## Run the command and log ### vaffanculo inglese di merda
+function run () {
+	echo "$1"
+	$1
+}
+
+## Create a virtua monitor
+run "xrandr --newmode $modeline"
+run "xrandr --addmode VIRTUAL1 $res"
+run "xrandr --output VIRTUAL1 --mode $mode --{$position}-of ${fisic}"
+
+## GOOD MAN!
+
+## Run the VNC server
+run "x11vnc -clip ${position} -xrandr" ##-ncache 1 -nosel -fixscreen \"V=2\" -noprimary -nosetclipboard -noclipboard -cursor arrow -nopw -nowf -nonap -noxdamage -sb 0 -display :0"
+
+## Turn AndroMonitor off
+run "xrandr --output $virtual --off"
+run "xrandr --delmode $virtual $mode"
+run "xrandr --rmmode $mode"
+run "xrandr -s 0"
